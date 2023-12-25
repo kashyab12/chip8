888a0bfab29490f6672959e9cb3d2c93718660ae
kashyab12 <iamkashyab@gmail.com>
2023-12-25 14:29:34 -0500

    SDL2 test window and updated reference guide for sdl2

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ A simple first emulator! Following the references below for the implementation:
 - [Chip 8 Technical Reference 1](https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference)
 - [Chip 8 Technical Reference 2](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
 - [Chip 8 Instruction Set](https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Instruction-Set)
+- [SDL2 Reference](https://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php)
 
 Follow me working on this live: "[emudev - chip8]()".
--- src/main.c
+++ src/main.c
@@ -3,8 +3,41 @@
 //
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello world!");
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
+    SDL_Window* window = NULL;
+    SDL_Surface* screen_surface = NULL;
+    // Init SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Init didn't work!");
+    }
+    const char* title = "Test out SDL";
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window creation failed!");
+    }
+    // Getting the window surface
+    screen_surface = SDL_GetWindowSurface(window);
+    // Color the surface
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+    // Updating surface
+    SDL_UpdateWindowSurface(window);
+    // Keeping the window up
+    SDL_Event event;
+    bool quit = false;
+    while (quit == false) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || (event.type == SDL_TEXTINPUT && tolower(event.text.text[0]) == 'x') ) {
+                quit = true;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
