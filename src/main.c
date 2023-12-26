7fb9bdd99b1e06b693ad090bb495a5d1ecc67d19
kashyab12 <iamkashyab@gmail.com>
2023-12-26 15:16:56 -0500

    initializing the chip8 struct

--- src/main.c
+++ src/main.c
@@ -5,19 +5,33 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+// Constants
+const uint16_t SCREEN_WIDTH = 640;
+const uint16_t SCREEN_HEIGHT = 480;
+const uint16_t MEMORY_SIZE = 4096;
+const uint8_t GENERAL_PURPOSE_REGS = 16;
+const uint8_t STACK_DEPTH = 16;
+const uint8_t NUM_KEYS = 16;
+const uint8_t VIEWPORT_WIDTH = 64;
+const uint8_t VIEWPORT_HEIGHT = 32;
 
-int main(int argc, char* args[]) {
-    SDL_Window* window = NULL;
-    SDL_Surface* screen_surface = NULL;
+struct chip8 {
+    uint8_t mmap[MEMORY_SIZE], v[GENERAL_PURPOSE_REGS], keyboard[NUM_KEYS], display[
+            VIEWPORT_HEIGHT * VIEWPORT_WIDTH], sound_timer, delay_timer, sp;
+    uint16_t pc, stack[STACK_DEPTH], opcode;
+};
+
+
+int main(int argc, char *args[]) {
+    SDL_Window *window = NULL;
+    SDL_Surface *screen_surface = NULL;
     // Init SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Init didn't work!");
     }
-    const char* title = "Test out SDL";
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    const char *title = "Test out SDL";
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Window creation failed!");
     }
@@ -32,7 +46,7 @@ int main(int argc, char* args[]) {
     bool quit = false;
     while (quit == false) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || (event.type == SDL_TEXTINPUT && tolower(event.text.text[0]) == 'x') ) {
+            if (event.type == SDL_QUIT || (event.type == SDL_TEXTINPUT && tolower(event.text.text[0]) == 'x')) {
                 quit = true;
             }
         }
