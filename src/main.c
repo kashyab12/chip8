35ddc91b970cc0328b9ea80535ed1954f14e4e5e
kashyab12 <iamkashyab@gmail.com>
2024-01-03 02:43:38 -0500

    draft of update real display function!

--- src/main.c
+++ src/main.c
@@ -63,13 +63,30 @@ void op_rand(struct chip8 *chip_ate); // RND instruction -> Vx = rand(0,255) & k
 void op_drw(struct chip8 *chip_ate); // DRW instruction -> display n-byte sprite starting at mem location I at (Vx, Vy), set VF = collision
 
 void init_sprite_data(struct chip8 *chip_ate);
+void ld_instructions_file(const char* file_path);
 instruction opcode_to_instruction(uint16_t opcode);
-void update_display(struct chip8 *chip_ate, SDL_Window* window, SDL_Surface *screen_surface); // Update the SDL display as per what's held in the chip8 display array
+void update_real_display(struct chip8 *chip_ate, SDL_Window* window, SDL_Surface *screen_surface); // Update the SDL display as per what's held in the chip8 display array
 void draw_sprite(struct chip8 *chip_ate, SDL_Window* window, SDL_Surface *screen_surface, int start_x, int start_y, const uint8_t sprite_bytes[], size_t num_sprite_bytes);
 size_t twod_to_oned_arr_idx(size_t twod_arr_max_rows, size_t twod_row, size_t twod_col);
 
 int main(int argc, char *args[]) {
-    struct chip8 *chip_ate = {};
+    // Initalizations - ChipAte and SDL
+    struct chip8 *chip_ate;
+    SDL_Window *window = NULL;
+    SDL_Surface *screen_surface = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Init didn't work!");
+    }
+    const char *title = "Test out SDL";
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window creation failed!");
+    }
+    screen_surface = SDL_GetWindowSurface(window);
+    // TODO: Load .8o file here?
+    ld_instructions_file(chip_ate);
+    // TODO: Need to reference the PC to know when we are done? Or maybe return it from ld_instructions
     bool final_instruction = false;
     // Fetch-Decode-Execute cycle
     while (!final_instruction) {
@@ -81,28 +98,16 @@ int main(int argc, char *args[]) {
         instruction func = opcode_to_instruction(fetched_op);
         // Execute Step - execute the relevant instruction
         (*func)(chip_ate);
+        // TODO: Optimize to call when required!
+        update_real_display(chip_ate, window, screen_surface);
     }
+    // // Color the surface
+    // int start_x = (int)(SCREEN_WIDTH / 2);
+    // int start_y = (int)(SCREEN_HEIGHT / 2);
+    // uint8_t zero_sprite[] = {0xF0, 0x90, 0x90, 0x90, 0xF0};
+    // struct chip8 chip_ate;
+    // draw_sprite(&chip_ate, window, screen_surface, start_x, start_y, zero_sprite, 5);
 
-    SDL_Window *window = NULL;
-    SDL_Surface *screen_surface = NULL;
-    // Init SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Init didn't work!");
-    }
-    const char *title = "Test out SDL";
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
-                              SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window creation failed!");
-    }
-    // Getting the window surface
-    screen_surface = SDL_GetWindowSurface(window);
-    // Color the surface
-    int start_x = (int)(SCREEN_WIDTH / 2);
-    int start_y = (int)(SCREEN_HEIGHT / 2);
-    uint8_t zero_sprite[] = {0xF0, 0x90, 0x90, 0x90, 0xF0};
-    struct chip8 chip_ate;
-    draw_sprite(&chip_ate, window, screen_surface, start_x, start_y, zero_sprite, 5);
     // Keeping the window up
     SDL_Event event;
     bool quit = false;
@@ -268,12 +273,36 @@ void init_sprite_data(struct chip8 *chip_ate) {
     chip_ate->mmap[+mem_start] = 0xF0;
 }
 
+void update_real_display(struct chip8 *chip_ate, SDL_Window* window, SDL_Surface *screen_surface) {
+    const int pixel_width = SCREEN_WIDTH / VIEWPORT_WIDTH;
+    const int pixel_height = SCREEN_HEIGHT / VIEWPORT_HEIGHT;
+    size_t scaled_y = 0;
+    for (size_t row_idx=0; row_idx < VIEWPORT_HEIGHT; row_idx+=1) {
+        size_t scaled_x = 0;
+        for (size_t col_idx=0; col_idx < VIEWPORT_WIDTH; col_idx+=1) {
+            size_t oned_idx = twod_to_oned_arr_idx(VIEWPORT_HEIGHT, row_idx, col_idx);
+            uint8_t color_val = chip_ate->display[oned_idx];
+            SDL_Rect pixel_fill_rect = { .x = scaled_x, .y = scaled_y, .w = pixel_width, .h = pixel_height};
+            if (color_val == 0) {
+                SDL_FillRect(screen_surface, &pixel_fill_rect, SDL_MapRGB(screen_surface->format, 0x0, 0x0, 0x0));
+            } else if (color_val == 1) {
+                SDL_FillRect(screen_surface, &pixel_fill_rect, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+            } else {
+                // Should not happen!
+                abort();
+            }
+            scaled_x += pixel_width;
+        }
+        scaled_y += pixel_height;
+    } 
+}
+
 // Draft function - some of this behavior will be transferred to the DRW instruction
 void
 draw_sprite(struct chip8 *chip_ate, SDL_Window* window, SDL_Surface *screen_surface, int start_x, int start_y, const uint8_t sprite_bytes[], size_t num_sprite_bytes) {
     int current_x, current_y;
-    int pixel_width = SCREEN_WIDTH / VIEWPORT_WIDTH;
-    int pixel_height = SCREEN_HEIGHT / VIEWPORT_HEIGHT;
+    const int pixel_width = SCREEN_WIDTH / VIEWPORT_WIDTH;
+    const int pixel_height = SCREEN_HEIGHT / VIEWPORT_HEIGHT;
     current_x = start_x;
     current_y = start_y;
     // TODO: Need to incorporate the display array via the chip_ate struct here
